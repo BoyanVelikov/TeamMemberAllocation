e18236e1972c12e243b6ae320d1f35c0f01670c7
Boyan Velikov <boyanvelikov@gmail.com>
2022-09-16 03:46:59 +0000

    Edit 1

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,3 +1,4 @@
+import Teams from './Teams';
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
 
@@ -7,12 +8,7 @@ const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeam
     <main className="container">
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-6'>
-          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
-            <option value="TeamA">TeamA</option>
-            <option value="TeamB">TeamB</option>
-            <option value="TeamC">TeamC</option>
-            <option value="TeamD">TeamD</option>
-          </select>
+          <Teams selectedTeam={selectedTeam} handleTeamSelectionChange={handleTeamSelectionChange}/>
         </div>
       </div>
 
