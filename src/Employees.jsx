eedd0ae78bce1fdb2838b62d471477c08c6a617a
Boyan Velikov <boyanvelikov@gmail.com>
2022-09-16 04:50:29 +0000

    Final version

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,6 +1,6 @@
 import Teams from './Teams';
-import femaleProfile from './images/femaleProfile.jpg';
-import maleProfile from './images/maleProfile.jpg';
+import TeamMembers from './TeamMembers';
+
 
 const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
 
@@ -11,25 +11,12 @@ const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeam
           <Teams selectedTeam={selectedTeam} handleTeamSelectionChange={handleTeamSelectionChange}/>
         </div>
       </div>
-
-
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <div className='card-collection'>
-            {
-              employees.map((employee) => (
-                <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
-
-                  {(employee.gender === 'male') ? <img src={maleProfile} className='card-img-top' />
-                    : <img src={femaleProfile} className='card-img-top' />}
-
-                  <div className='card-body'>
-                    <h5 className='card-title'>Full name: {employee.fullName}</h5>
-                    <p className='card-text'><b>Designation:</b>{employee.designation}</p>
-                  </div>
-                </div>
-              ))
-            }
+              <TeamMembers employees={employees}
+                handleEmployeeCardClick={handleEmployeeCardClick}
+                selectedTeam={selectedTeam}/>
           </div>
         </div>
       </div>
